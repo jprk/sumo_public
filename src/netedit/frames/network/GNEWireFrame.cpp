01055c0adbf4a85532b0ea07bdc1221474e6e142
Jan Přikryl <prikryl@fel.zcu.cz>
2025-02-03 17:32:20 +0100

    Message text correction.

--- src/netedit/frames/network/GNEWireFrame.cpp
+++ src/netedit/frames/network/GNEWireFrame.cpp
@@ -66,7 +66,7 @@ GNEWireFrame::show() {
     // show frame
     GNEFrame::show();
     if (!myWarnedExperimental) {
-        FXMessageBox::warning(getApp(), MBOX_OK, TL("Experimental Part"), "%s", TL("Warning: The netedit overhead editor is still in experimental state."));
+        FXMessageBox::warning(getApp(), MBOX_OK, TL("Experimental Part"), "%s", TL("Warning: The netedit overhead wire editor is still in experimental state."));
         myWarnedExperimental = true;
     }
 }
